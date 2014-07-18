b206cefbf65b6b88de27e582da3a276d5d055fcc
jennifer <jennifer@jennifer-desktop.(none)>
2014-07-18 15:14:59 -0430

    codigo arduino escucha serial

---
+++ detectarBlobYarduino/arduinoEscucha/arduinoEscucha.ino
@@ -0,0 +1,20 @@
+
+int led = 13;
+
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);  
+  Serial.begin(9600);  
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  if(Serial.available() > 0){
+     if(Serial.read()== 'y'){
+     digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+     delay(1000);               // wait for a second
+     digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+     delay(1000);               // wait for a second
+    }
+  }
+}

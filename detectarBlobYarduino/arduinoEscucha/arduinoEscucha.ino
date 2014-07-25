65e3fb8440d66dbc0be3b692c3e0449e58ef221b
jenniferdr <jenniferdosreis@hotmail.com>
2014-07-25 14:20:54 -0430

    Arduino blink

--- detectarBlobYarduino/arduinoEscucha/arduinoEscucha.ino
+++ detectarBlobYarduino/arduinoEscucha/arduinoEscucha.ino
@@ -12,9 +12,8 @@ void loop() {
   if(Serial.available() > 0){
      if(Serial.read()== 'y'){
      digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-     delay(1000);               // wait for a second
-     digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-     delay(1000);               // wait for a second
+    }else{
+     digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW   
     }
   }
 }
